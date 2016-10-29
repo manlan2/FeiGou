60d2bae03e586ef0cd752a99f637304e3655be86
“klike” <2283529034@qq.com>
2016-10-29 11:51:37 +0800

    home

--- FeiGou/Home/HomeTableViewController.swift
+++ FeiGou/Home/HomeTableViewController.swift
@@ -17,25 +17,29 @@ class HomeListCell: UITableViewCell {
 }
 
 class CategoryCell: UICollectionViewCell {
-    
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var title: UILabel!
-
 }
 
 class HomeTableViewController: UITableViewController , BannerViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate{
     
     var listData: ProductModel?
     var categoryArr: [categoryListModel]?
+    let listName = ["苹果苹果苹果苹果苹果苹果苹果苹果苹果苹果", "香蕉香蕉香蕉香蕉香蕉香蕉香蕉", "栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子", "哈密瓜哈密瓜哈密瓜哈密瓜", "苹果苹果苹果苹果苹蕉香蕉", "香蕉香蕉香蕉香蕉香蕉香蕉香蕉", "栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子", "栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子", "栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子栗子", "果苹果苹果苹果苹蕉香蕉子栗子栗子栗子栗子栗子栗子栗子"]
+    let listPrice = [12,321,3123,21,31,3,32,3,11,65]
     
     lazy var categoryView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize.init(width: (screenW - 100) / 5, height: (screenW - 100) / 5 + 30)
-        let cview = UICollectionView.init(frame: CGRect.init(x: 0, y: screenW / 2.35, width: screenW, height: screenW / 2.35), collectionViewLayout: layout)
+        layout.itemSize = CGSize.init(width: screenW / 5, height: screenW / 5 + 10)
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        let cview = UICollectionView.init(frame: CGRect.init(x: 0, y: screenW / 2.35, width: screenW, height: screenW / 5 * 2 + 20), collectionViewLayout: layout)
         cview.delegate = self
         cview.dataSource = self
         cview.isScrollEnabled = false
-        cview.register(CategoryCell.self, forCellWithReuseIdentifier: "CategoryCell")
+        cview.backgroundColor = UIColor.clear
+//        cview.register(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        cview.register(UINib.init(nibName: "CategoryCell", bundle: nil), forCellWithReuseIdentifier: "CategoryCell")
         return cview
     }()
     
@@ -43,7 +47,7 @@ class HomeTableViewController: UITableViewController , BannerViewDelegate, UICol
         let bannerArr = ["http://120.24.3.172:8080/feigou-api/api/upload/images/index/banner/home_banner1.png",
                          "http://120.24.3.172:8080/feigou-api/api/upload/images/index/banner/home_banner2.png",
                          "http://120.24.3.172:8080/feigou-api/api/upload/images/index/banner/home_banner3.png",]
-        let view = UIView.init(frame: CGRect.init(x: 0, y: 0, width: screenW, height: screenW / 2.35 + ((screenW - 100) / 5 + 30) * 2.5))
+        let view = UIView.init(frame: CGRect.init(x: 0, y: 0, width: screenW, height: screenW / 2.35 + screenW / 5 * 2 + 20))
         let bannerView = BannerView.creatBanner(frame: CGRect.init(x: 0, y: 0, width: screenW, height: screenW / 2.35), imgUrlArr: bannerArr, delegate: self)
         view.addSubview(bannerView)
         view.addSubview(self.categoryView)
@@ -61,7 +65,7 @@ class HomeTableViewController: UITableViewController , BannerViewDelegate, UICol
         }
         FeiGouApi.shareInstance.getHomeCategory { (respondModel) in
             self.categoryArr = respondModel.categoryList
-            self.tableView.reloadData()
+            self.categoryView.reloadData()
         }
     }
 
@@ -81,18 +85,30 @@ class HomeTableViewController: UITableViewController , BannerViewDelegate, UICol
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         if listData != nil {
-            return listData!.productList!.count
+//            return listData!.productList!.count
+            return 10
         }
         return 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "homelistcell", for: indexPath) as! HomeListCell
-        let model: productListModel = listData!.productList![indexPath.row]
-        cell.title.text = model.name!
-        cell.price.text = "P\(model.price!)"
-        cell.img.kf_setImage(with: URL.init(string: model.picPath!))
+//        let model: productListModel = listData!.productList![indexPath.row]
+        cell.title.text = listName[indexPath.row]
+        cell.state.text = "接受预订"
+        cell.price.text = "P\(listPrice[indexPath.row])"
+        cell.img.kf_setImage(with: URL.init(string: "https://source.unsplash.com/random/300x(\(200+indexPath.row))"))
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView.init(frame: CGRect.init(x: 0, y: 0, width: screenW, height: 8))
+        view.backgroundColor = UIColor.colorFromHex(0xededed)
+        return view
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 8
     }
     
     // MARK: - collect view data source
@@ -103,14 +119,16 @@ class HomeTableViewController: UITableViewController , BannerViewDelegate, UICol
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = self.categoryView.dequeueReusableCell(withReuseIdentifier: "CategoryCell", for: indexPath) as! CategoryCell
-        let model = self.categoryArr?[indexPath.row]
-//        cell.title.text = model?.name
-//        cell.img.kf_setImage(with: URL.init(string: (model?.icon)!))
+        if self.categoryArr != nil {
+            let model: categoryListModel = (self.categoryArr?[indexPath.row])!
+            cell.title.text = model.name
+            cell.img.kf_setImage(with: URL.init(string: (model.icon)!))
+        }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
+        print(indexPath)
     }
     
 }
